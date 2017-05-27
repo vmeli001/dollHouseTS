8415c17ec84a71d08d86f7b6b7712f9f6c6195ad
vmeli001 <meligne.v@gmail.com>
2017-05-26 22:26:01 -0700

    Update and rename dollHouseTouchScreen.h to Arduino/dollHouseTouchScreen.h

---
+++ Arduino/dollHouseTouchScreen.h
@@ -0,0 +1,11 @@
+// dollHouseTouchScreen.h
+//
+// Touchscreen routines for the doll house project
+// By Vincent Meligne
+
+#include "TouchScreen.h"
+#include "DollHouseTS.h"
+#include <EEPROM.h>
+
+void calibration();
+void touchLoc(TSPoint p);
