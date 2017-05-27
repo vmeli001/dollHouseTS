76898497aaba19ce2c223d39b0da7f59a6b1edb8
vmeli001 <meligne.v@gmail.com>
2017-05-26 22:23:58 -0700

    Update and rename dollHouseColor.h to Arduino/dollHouseColor.h

---
+++ Arduino/dollHouseColor.h
@@ -0,0 +1,11 @@
+// dollHouseColor.h
+//
+// Header file for the dollHouse color functions
+// By Vincent Meligne
+
+void checkBalance();
+void setBalance();
+void checkColor();
+void getReading(int times);
+void sendColor(String where);
+void updateLigths(String where, String color);
